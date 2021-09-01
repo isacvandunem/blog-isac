0b268390ac2aa28391bd142ea3f24c33fca70b19
Isac <isacvandunem@gmail.com>
2021-09-01 19:24:23 +0100

    adds a loader component and uses it in the posts lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PostComponent } from './post/post.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsListComponent,
     NavBarComponent,
-    PostComponent
+    PostComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post/post.model';
+import { APIService } from '../services/APIService';
 
 @Component({
   selector: 'app-posts-list',
@@ -8,11 +9,15 @@ import { Post } from '../post/post.model';
 })
 export class PostsListComponent implements OnInit {
   public posts: Post[] = [];
+  public loading = false;
 
-  constructor() { 
-
-  }
+  constructor(private apiService: APIService) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.apiService.getPosts().subscribe(posts => {
+      this.posts = posts;
+      this.loading = false;
+    });
   }
 }
